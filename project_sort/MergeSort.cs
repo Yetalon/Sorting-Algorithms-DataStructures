53c13604326fddb285fb2bdca377d1ff825a4cbd
Tanyan9918 <tanyanpollard9918@gmail.com>
2025-02-25 10:57:54 -0500

    Created Outlines for Algorithms
    Barebone algorithm classes

---
+++ project_sort/MergeSort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_sort
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class MergeSort<T> : ISort<T> where T : IComparable<T>
+    {
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="values"></param>
+        public void Sort(List<T> values)
+        {
+            
+        }
+    }
+}
