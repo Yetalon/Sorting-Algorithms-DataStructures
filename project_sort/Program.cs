cd2c374f79345656ba4f15e4085a381308d2ffa0
Yetalon <Davisac7@etsu.edu>
2025-02-26 15:44:11 -0500

    Did test cases for data for insertion sort
    put the test data in its own file and created some charts rq
    Setup program.cs to look cleaner

--- project_sort/Program.cs
+++ project_sort/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Text.RegularExpressions;
 
 namespace project_sort
 {
@@ -7,26 +6,74 @@ namespace project_sort
     {
         static void Main(string[] args)
         {
-            /*
-            string line = "| Lewis       | Sophia       | Lost and Found Coastal Picturesque Welcoming Maplewood Meadows | 1981-09-16     |";
-            Book book = new();
-            
-            if(book.TryParse(line, out book)) {
-                Console.WriteLine(book.ToString());
-            }
-            else {
-                Console.WriteLine("Failure");
-            }
-            */
+            //InsertionDataInts(args);
+            //InsertionDataBooks(args);
+        }
 
-          /*   Stopwatch stopwatch = new();
-            List<int> items = [12, 11, 13, 5, 6];
-            InsertionSort<int> insertionSort = new();
-            stopwatch.Start();
-            insertionSort.Sort(items);
-            stopwatch.Stop();
-            Console.WriteLine(stopwatch.ElapsedMilliseconds);
-            stopwatch.Reset(); */
+        public static void InsertionDataBooks(string[] args) {
+            string fileToWriteTo = "./insertion_data_books.txt";
+            if (args.Length > 0) {
+                foreach (var item in args) {
+                    string filename = item;
+                    List<Book> books = LoadBookTestData(item);
+                    Stopwatch stopwatch = new();
+                    InsertionSort<Book> insertionSort = new();
+                    stopwatch.Start();
+                    insertionSort.Sort(books);
+                    stopwatch.Stop();
+                    var time = stopwatch.ElapsedMilliseconds;
+                    stopwatch.Reset();
+                    using (StreamWriter writer = new(fileToWriteTo, true)) {
+                        writer.WriteLine($"{filename},{time}");
+                    }
+                }
+            }
+        }
+        public static void InsertionDataInts(string[] args) {
+            string fileToWriteTo = "./insertion_data_ints.txt";
+            if (args.Length > 0) {
+                foreach (var item in args) {
+                    string filename = item;
+                    List<int> items = LoadIntegerTestData(item);
+                    Stopwatch stopwatch = new();
+                    InsertionSort<int> insertionSort = new();
+                    stopwatch.Start();
+                    insertionSort.Sort(items);
+                    stopwatch.Stop();
+                    var time = stopwatch.ElapsedMilliseconds;
+                    stopwatch.Reset();
+                    using (StreamWriter writer = new(fileToWriteTo, true)) {
+                        writer.WriteLine($"{filename},{time}");
+                    }
+                }
+            }
+        }
+        
+        public static List<Book> LoadBookTestData (string filePath) {
+            List<Book> books = [];
+            using (StreamReader reader = new(filePath)) {
+                string? line;
+                while ((line = reader.ReadLine()) != null) {
+                    Book book = new();
+                    if (book.TryParse(line, out book)) {
+                        books.Add(book);
+                    }
+                }
+            }
+            return books;
+        }
+
+        public static List<int> LoadIntegerTestData(string filePath) {
+            List<int> ints = [];
+            using (StreamReader reader = new(filePath)) {
+                string? line;
+                while ((line = reader.ReadLine()) != null) {
+                    if (int.TryParse(line, out int value)) {
+                        ints.Add(value);
+                    }
+                }
+            }
+            return ints;
         }
     }
 }
