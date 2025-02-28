240f269fc325f0784fa3774ed8862ba8572bc29d
Tanyan9918 <tanyanpollard9918@gmail.com>
2025-02-27 19:13:29 -0500

    Still Working on Merge Sort
    Scared my laptop is gonna die soon, Just have to make the merge method then it will be time to test it

--- project_sort/MergeSort.cs
+++ project_sort/MergeSort.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,9 +26,30 @@ namespace project_sort
                 return;
             }
             int mid = (values.Count) / 2;
-            List<T> values1 = new List<T>();
-            List<T> values2 = new List<T>();
-
+            List<T> valuesLeft = values.GetRange(0, mid);
+            List<T> valuesRight = values.GetRange(mid, values.Count - mid);
+            Sort(valuesLeft);
+            Sort(valuesRight);
+            Merge(values, valuesLeft, valuesRight);
         }
+
+        /// <summary>
+        /// Merges the values together
+        /// </summary>
+        /// <param name="values">The original list</param>
+        /// <param name="valuesLeft">The left list</param>
+        /// <param name="valuesRight">The right list</param>
+        private void Merge(List<T> values, List<T> valuesLeft, List<T> valuesRight)
+        {
+            int i = 0;
+            while(valuesLeft.Count > 0 && valuesRight.Count > 0)
+            {
+                if(Comparer<T>.Default.Compare(valuesLeft, valuesRight))
+                {
+
+                }
+            }
+        }
+
     }
 }
